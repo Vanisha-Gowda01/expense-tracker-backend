054374c1e2bc765b4e352fce2a70c4953b00e0cd
Vanisha-Gowda01 <vanishagowdavanishagowda@gmail.com>
2026-05-26 15:01:49 +0530

    Updated backend with JWT auth and frontend integration

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.database import Base, engine
 
-from app.routers.auth_router import router as auth_router
 from app.routers.user_router import router as user_router
 from app.routers.expense_router import router as expense_router
 from app.routers.income_router import router as income_router
 from app.routers.analytics_router import router as analytics_router
-
+from app.routers.auth_router import router as auth_router
 
 from app.models.user_model import User
 from app.models.expense_model import Expense
@@ -16,12 +17,24 @@ from app.models.income_model import Income
 
 Base.metadata.create_all(bind=engine)
 
+
 app = FastAPI(
     title="Expense Tracker Backend"
 )
-app.include_router(expense_router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_router)
+
+app.include_router(expense_router)
 
 app.include_router(income_router)
 
@@ -29,8 +42,10 @@ app.include_router(analytics_router)
 
 app.include_router(auth_router)
 
+
 @app.get("/")
 def home():
+
     return {
         "message": "Backend Running Successfully"
     }
